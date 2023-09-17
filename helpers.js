47a8cd54b8dff9f47e0d28661fadb6c3eb72e955
KilzaNiko <nicolasmerca@gmail.com>
2023-09-17 18:35:32 -0300

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@
 //a_letter = solo devuelve las letras en un array y en formato string
 //letter = este parametro recibe la letra a consultar, debe ir especificada si o si junto a s_letter=true
 function get_data_table(all=true, a_letter=false, letter){
-    if ((all && a_letter) || (!all && !a_letter && letter === null) || (typeof letter !== "string" && letter.length === 1 && !/[B|I|N|G|O]/.test(letter))){ return false } 
+    if ((all && a_letter) || (!all && !a_letter && letter === null) || typeof letter !== "string" || letter.length !== 1 || !/[B|I|N|G|O]/.test(letter)){ return false } 
     const N1 = Array.from({ length: 15 }, (_, index) => index + 1);
     const N2 = Array.from({ length: 15 }, (_, index) => index + 16);
     const N3 = Array.from({ length: 15 }, (_, index) => index + 31);
