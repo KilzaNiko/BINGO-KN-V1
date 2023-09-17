bc03c7a1a33a0352ad7d7439635420be73145593
KilzaNiko <nicolasmerca@gmail.com>
2023-09-17 17:49:08 -0300

    &&

--- helpers.js
+++ helpers.js
@@ -2,9 +2,9 @@
 //all = devuelve todo el obj con letras y sus respectivos numeros
 //a_letter = solo devuelve las letras en un array y en formato string
 //s_letter = devuelve una letra especifica junto a sus respectivos numeros
-//letter = este parametro recibe la letra a consultar, debe ir especificada si o si jutno a s_letter=true
+//letter = este parametro recibe la letra a consultar, debe ir especificada si o si junto a s_letter=true
 function get_data_table(all=true, a_letter=false, s_letter=false, letter){
-    if (!all){ if ((a_letter & s_letter) || (!a_letter & !s_letter) || (typeof letter === "string" && letter.length === 1 && /[B|I|N|G|O]/.test(letter.toString()))){ return false } }
+    if (!all){ if ((a_letter && s_letter) || (!a_letter && !s_letter && !all) || (typeof letter === "string" && letter.length === 1 && /[B|I|N|G|O]/.test(letter.toString()))){ return false } }
     const N1 = Array.from({ length: 15 }, (_, index) => index + 1);
     const N2 = Array.from({ length: 15 }, (_, index) => index + 16);
     const N3 = Array.from({ length: 15 }, (_, index) => index + 31);
