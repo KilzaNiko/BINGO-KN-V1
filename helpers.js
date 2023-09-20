aded5ced1ba3931d5bb4ce9afd2efb1657040db9
KilzaNiko <nicolasmerca@gmail.com>
2023-09-20 10:32:43 -0300

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -38,20 +38,18 @@ function generate_vtable() {
     const letters = ['B', 'I', 'N', 'G', 'O'];
     const letterColors = { 'B': "primary", 'I': "success", 'N': "warning", 'G': "danger", 'O': "info" };
 
-    for (let row = 0; row < 16; row++) {
-        document.write('<tr>');
-        for (let col = 0; col < 5; col++) {
-            if (row === 0) {
-                const letter = letters[col];
-                const colorClass = letterColors[letter];
-                document.write(`<td class="table-${colorClass}" id="${letter}">${letter}</td>`);
-            } else {
-                const number = row + (col * 16);
-                document.write(`<td class="number" id="${number}">${number}</td>`);
-            }
+    document.write('<tr>');
+    for (let col = 0; col < 5; col++) {
+        if (row === 0) {
+            const letter = letters[col];
+            const colorClass = letterColors[letter];
+            document.write(`<td class="table-${colorClass}" id="${letter}">${letter}</td>`);
+        } else {
+            const number = row + (col * 16);
+            document.write(`<td class="number" id="${number}">${number}</td>`);
         }
-        document.write('</tr>');
     }
+    document.write('</tr>');
 }
 
 
