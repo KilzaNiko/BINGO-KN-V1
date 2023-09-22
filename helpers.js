92359d8b4d42680ba006bbe8bf6583c67ccbd47c
KilzaNiko <nicolasmerca@gmail.com>
2023-09-22 15:02:35 -0300

    a

--- helpers.js
+++ helpers.js
@@ -3,8 +3,8 @@
 //a_letter = solo devuelve las letras en un array y en formato string
 //letter = devuelve los numeros correspondientes a la letra especificada
 //la validacion de las 3 variables tienen sentido de izquierda a derecha
-function get_data_table(all=true, a_letter=false, letter){
-    if(all && a_letter || !all && !a_letter && !letter || !all && !a_letter && (typeof letter !== "string" || letter.length > 1 || !/[B|I|N|G|O]/.test(letter.toLocaleUpperCase())) ){ return false }
+function get_data_table(all=true, a_letter=false, letter, v_data=false){
+    if(all && a_letter || !all && !a_letter && !letter && !v_data || !all && !a_letter && (typeof letter !== "string" || letter.length > 1 || !/[B|I|N|G|O]/.test(letter.toLocaleUpperCase())) ){ return false }
 
     const N1 = Array.from({ length: 15 }, (_, index) => index + 1);
     const N2 = Array.from({ length: 15 }, (_, index) => index + 16);
@@ -15,10 +15,11 @@ function get_data_table(all=true, a_letter=false, letter){
     const table_obj = { B: N1, I: N2, N: N3, G: N4, O: N5 };
 
     if(all){return table_obj}
-    else{
-        if(a_letter){return Object.keys(table_obj)}
-        else{
-            return table_obj[(letter === letter.toLowerCase()) ? letter.toUpperCase() : letter] 
+    else if(a_letter){return Object.keys(table_obj)}
+    else if(letter === "string"){return table_obj[(letter === letter.toLowerCase()) ? letter.toUpperCase() : letter]}
+    else if(v_data){ 
+        const v_table_obj = {
+            
         }
     }
 }
