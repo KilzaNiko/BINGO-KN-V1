7a0a23fee3ce5d993e815fd6600b8c754ed71492
KilzaNiko <nicolasmerca@gmail.com>
2023-09-26 10:13:54 -0300

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -58,12 +58,9 @@ function generate_vertical_table(){
         document.write('<tr>');
         for (x = 0; x < 5; x++){
             if ( i === 0 ) {
-                console.log(x)
                 document.write(`<td class="table-${letterColors[x]}" id="${v_data["BINGO"][x]}">${v_data["BINGO"][x]}</td>`);
             }else{
-                console.log("numero i - " + i)
-                console.log("numero x - " + x)
-                document.write(`<td class="number">${v_data[eval(`F${i}`)[x]]}</td>`);
+                document.write(`<td class="number">${v_data[`F${i}`[x]]}</td>`);
             }
         }
         document.write('</tr>');
