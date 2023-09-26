00e73aa0a89b7671494fd4b5c71606f3d40ea876
KilzaNiko <nicolasmerca@gmail.com>
2023-09-26 12:13:10 -0300

    d

--- helpers.js
+++ helpers.js
@@ -4,3 +4,18 @@ function getWindowsSize() {
     
     return [ancho, alto];
 }
+
+// Función para verificar y cambiar entre las tablas según la resolución de la pantalla
+function checkTableLayout() {
+    const windowWidth = window.innerWidth;
+    const horizontalTable = document.getElementById('horizontalTable');
+    const verticalTable = document.getElementById('verticalTable');
+
+    if (windowWidth >= 768) {
+        horizontalTable.style.display = 'block';
+        verticalTable.style.display = 'none';
+    } else {
+        horizontalTable.style.display = 'none';
+        verticalTable.style.display = 'block';
+    }
+}
