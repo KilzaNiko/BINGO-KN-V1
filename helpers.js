429cd36413c43147afc1628d66b6ff6ac86678ec
KilzaNiko <nicolasmerca@gmail.com>
2023-09-26 15:21:55 -0300

    j

--- helpers.js
+++ helpers.js
@@ -69,14 +69,3 @@ function checkTableLayout() {
     }
 }
 
-// Agregar eventos clic a las celdas de ambas tablas
-document.querySelectorAll('.number').forEach(cell => {
-    cell.addEventListener('click', () => {
-        const cellId = cell.id.replace('horizontal_', '').replace('vertical_', '');
-        if (cell.classList.contains('active')) {
-            deactivateCell(cellId);
-        } else {
-            activateCell(cellId);
-        }
-    });
-});
