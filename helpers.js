fded54b77d57831f4b0cdac2c47459addafc74fa
KilzaNiko <nicolasmerca@gmail.com>
2023-09-26 12:55:20 -0300

    a

--- helpers.js
+++ helpers.js
@@ -5,6 +5,48 @@ function getWindowsSize() {
     return [ancho, alto];
 }
 
+const undoStack = [];
+function changeCellState() {
+    const input = document.getElementById('cellInput').value;
+    const cellId = input.replace(/\D/g, ''); // Obtener solo los dígitos del input
+    const cell = document.getElementById(cellId);
+
+    if (cell) {
+        cell.classList.toggle('active'); // Cambiar el estado
+        undoStack.push(cellId); // Agregar a la pila para deshacer
+    }
+
+    document.getElementById('cellInput').value = ''; // Limpiar el input 
+}
+
+function resetAllCells() {
+    numberCells.forEach(cell => {
+        cell.classList.remove('active'); // Restablecer todas las celdas
+    });
+}
+
+// Estado de las celdas activas
+const activeCells = new Set();
+
+// Función para guardar el estado de las celdas activas
+function saveActiveCells() {
+    activeCells.clear();
+    const numberCells = document.querySelectorAll('.number.active');
+    numberCells.forEach(cell => {
+        activeCells.add(cell.id);
+    });
+}
+
+// Función para restaurar el estado de las celdas activas
+function restoreActiveCells() {
+    activeCells.forEach(cellId => {
+        const cell = document.getElementById(cellId);
+        if (cell) {
+            cell.classList.add('active');
+        }
+    });
+}
+
 // Función para verificar y cambiar entre las tablas según la resolución de la pantalla
 function checkTableLayout() {
     const windowWidth = window.innerWidth;
@@ -14,8 +56,16 @@ function checkTableLayout() {
     if (windowWidth >= 768) {
         horizontalTable.style.display = 'block';
         verticalTable.style.display = 'none';
+        restoreActiveCells(); // Restaurar el estado de las celdas activas en la tabla horizontal
     } else {
         horizontalTable.style.display = 'none';
         verticalTable.style.display = 'block';
+        saveActiveCells(); // Guardar el estado de las celdas activas en la tabla vertical
     }
 }
+
+// Ejecuta la función al cargar la página y cuando cambie el tamaño de la ventana
+window.addEventListener('load', checkTableLayout);
+window.addEventListener('resize', checkTableLayout);
+
+
